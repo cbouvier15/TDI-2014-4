daa2a3627f382a7882da7ca2918db723394325c9
Christian Bouvier <cbouvier@trea.uy>
2014-09-07 16:16:22 -0300

    Agrego Amnesia con imagen de presentacion y nombre actualizado.

---
+++ Amnesia/src/ofApp.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxAndroid.h"
+
+#include "ofxAccelerometer.h"
+
+class ofApp : public ofxAndroidApp{
+	
+	public:
+		
+		void setup();
+		void selectImage();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void windowResized(int w, int h);
+
+		void touchDown(int x, int y, int id);
+		void touchMoved(int x, int y, int id);
+		void touchUp(int x, int y, int id);
+		void touchDoubleTap(int x, int y, int id);
+		void touchCancelled(int x, int y, int id);
+		void swipe(ofxAndroidSwipeDir swipeDir, int id);
+
+		void pause();
+		void stop();
+		void resume();
+		void reloadTextures();
+
+		bool backPressed();
+		void okPressed();
+		void cancelPressed();
+
+		ofImage image, presentationImage;
+		ofVec3f accel;
+		ofDirectory imagesDir;
+		string imagePath;
+
+        void explosionGlitch(ofPixels pixels);
+        void stretchGlitch(ofPixels pixels);
+        void colorGlitch(ofPixels pixels);
+
+        bool shock,glitch, presentation;
+
+        int counter, presentationCounter;
+
+        int r,g,b;
+};
