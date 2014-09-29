4af235a44a0747c693da033ac8a95df5f4a51c71
Matias Nieves <matinieves@gmail.com>
2014-09-29 17:47:12 -0300

    Add EmTrack Adium Plug-in

---
+++ Laboratorio2/EmTrack/EmTrack.h
@@ -0,0 +1,14 @@
+//
+//  EmTrack.h
+//  EmTrack
+//
+//  Created by Matias Nieves on 9/29/14.
+//  Copyright (c) 2014 fing. All rights reserved.
+//
+
+
+#import <Adium/AIPlugin.h>
+
+@interface EmTrack : NSObject <AIPlugin>
+
+@end
